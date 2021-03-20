d6520b0f6f06ecb1ed08890a8dea57613db79da0
Sébastien Collard <sebastien.collard@epitech.eu>
2021-03-20 22:05:28 +0400

    handle win and reinit the game

--- App.tsx
+++ App.tsx
@@ -17,7 +17,16 @@ export default class App extends React.Component {
         }
     }
 
-    renderIcon(row, col) {
+    initGame() {
+        var board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+        this.setState({ gameBoard: board });
+    }
+
+    renderIcon(row:number, col:number) {
         var value:number = this.state.gameBoard[row][col];
 
         switch(value) {
@@ -27,9 +36,9 @@ export default class App extends React.Component {
         }
     }
 
-    onPressTile(row, col) {
-        console.log("ROW ::: ", row)
-        console.log("COL ::: ", col)
+    onPressTile(row:number, col:number) {
+        // console.log("ROW ::: ", row)
+        // console.log("COL ::: ", col)
 
         var board = this.state.gameBoard;
         if (board[row][col] == 0) {
@@ -37,6 +46,55 @@ export default class App extends React.Component {
             this.setState({ gmaeBoard: board });
             this.setState({ currentPlayer: this.state.currentPlayer * -1 });
         }
+        var win:number = this.winChecker();
+        if (win == 1) {
+            alert("Player X is the winner !");
+            this.initGame();
+        } else if (win == -1) {
+            alert("Player O is the winner !");
+            this.initGame();
+        }
+    }
+
+    winChecker():number {
+        // console.log("JE SUIS LE WIN CHECKER");
+        var board = this.state.gameBoard;
+
+        var rowSum:number = 0;
+        for (var rowI:number = 0; rowI < 3; rowI++) {
+            rowSum = board[rowI][0] + board[rowI][1] + board[rowI][2];
+            if (rowSum == 3) {
+                console.log("ROW X win this game !");
+                return 1;
+            } else if (rowSum == -3) {
+                console.log("ROW O win this game !");
+                return -1;
+            }
+        }
+
+        var colSum:number = 0;
+        for (var colI:number = 0; colI < 3; colI++) {
+            colSum = board[0][colI] + board[1][colI] + board[2][colI];
+            if (colSum == 3) {
+                console.log("COL X win this game !");
+                return 1;
+            } else if (colSum == -3) {
+                console.log("COL O win this game !");
+                return -1;
+            }
+        }
+
+        var diagonalL:number = board[0][0] + board[1][1] + board[2][2];
+        var diagonalR:number = board[0][2] + board[1][1] + board[2][0];
+        if (diagonalL == 3 || diagonalR == 3) {
+            console.log("Diagonal X win this game !");
+            return 1;
+        } else if (diagonalL == -3 || diagonalR == -3) {
+            console.log("Diagonal O win this game !");
+            return -1;
+        }
+
+        return 0;
     }
 
     render() {
@@ -79,6 +137,8 @@ export default class App extends React.Component {
                     { this.renderIcon(2,2) }
                 </TouchableOpacity>
             </View>
+
+            <Text></Text>
 
         </View>
         );
