cbba8aa1cc0c77440cf8fbd1060985d269d56689
Sébastien Collard <sebastien.collard@epitech.eu>
2021-03-20 20:52:34 +0400

    user can touch to place is pawn and display it

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Ionicons as Icon } from 'react-native-vector-icons';
 
 export default class App extends React.Component {
@@ -27,45 +27,57 @@ export default class App extends React.Component {
         }
     }
 
+    onPressTile(row, col) {
+        console.log("ROW ::: ", row)
+        console.log("COL ::: ", col)
+
+        var board = this.state.gameBoard;
+        if (board[row][col] == 0) {
+            board[row][col] = this.state.currentPlayer;
+            this.setState({ gmaeBoard: board });
+            this.setState({ currentPlayer: this.state.currentPlayer * -1 });
+        }
+    }
+
     render() {
         return (
         <View style={styles.container}>
             <StatusBar style="auto" />
 
             <View style={styles.rowTile}>
-                <View style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0, borderColor: "#000" }]}>
+                <TouchableOpacity onPress={() => this.onPressTile(0, 0)} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0, borderColor: "#000" }]}>
                     { this.renderIcon(0,0) }
-                </View>
-                <View style={[styles.tile, { borderTopWidth: 0 }]}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(0, 1)} style={[styles.tile, { borderTopWidth: 0 }]}>
                     { this.renderIcon(0,1) }
-                </View>
-                <View style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0 }]}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(0, 2)} style={[styles.tile, { borderRightWidth: 0, borderTopWidth: 0 }]}>
                     { this.renderIcon(0,2) }
-                </View>
+                </TouchableOpacity>
             </View>
             
             <View style={styles.rowTile}>
-                <View style={[styles.tile, { borderLeftWidth: 0 }]}>
+                <TouchableOpacity onPress={() => this.onPressTile(1, 0)} style={[styles.tile, { borderLeftWidth: 0 }]}>
                     { this.renderIcon(1,0) }
-                </View>
-                <View style={styles.tile}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(1, 1)} style={styles.tile}>
                     { this.renderIcon(1,1) }
-                </View>
-                <View style={[styles.tile, { borderRightWidth: 0}]}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(1, 2)} style={[styles.tile, { borderRightWidth: 0}]}>
                     { this.renderIcon(1,2) }
-                </View>
+                </TouchableOpacity>
             </View>
             
             <View style={styles.rowTile}>
-                <View style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0 }]}>
+                <TouchableOpacity onPress={() => this.onPressTile(2, 0)} style={[styles.tile, { borderLeftWidth: 0, borderBottomWidth: 0 }]}>
                     { this.renderIcon(2,0) }
-                </View>
-                <View style={[styles.tile, { borderBottomWidth: 0 }]}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(2, 1)} style={[styles.tile, { borderBottomWidth: 0 }]}>
                     { this.renderIcon(2,1) }
-                </View>
-                <View style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.onPressTile(2, 2)} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
                     { this.renderIcon(2,2) }
-                </View>
+                </TouchableOpacity>
             </View>
 
         </View>
